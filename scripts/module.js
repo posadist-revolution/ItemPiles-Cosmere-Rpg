e1e3c4fc0bd2be7c631411f2ed974fb759f87ee1
boxfriend <cawills394@gmail.com>
2025-10-24 15:27:40 -0400

    registers with libWrapper to allow turning any token into an item pile

--- scripts/module.js
+++ scripts/module.js
@@ -3,7 +3,7 @@ import getCurrency from "./currency.js";
 Hooks.once('item-piles-ready', async function() {
 	const currencies =  await getCurrency();
     const config = {
-		"VERSION": "1.1.2",
+		"VERSION": "1.2.0",
         "ACTOR_CLASS_TYPE": "adversary",
         "ITEM_CLASS_LOOT_TYPE": "loot",
         "ITEM_CLASS_WEAPON_TYPE": "weapon",
@@ -19,6 +19,31 @@ Hooks.once('item-piles-ready', async function() {
             }
         ],
 		"UNSTACKABLE_ITEM_TYPES": ["weapon", "armor"],
+
+		"SHEET_OVERRIDES": () => {
+			const sheetOverrides = Object.keys(CONFIG.Actor.sheetClasses).map(str => {
+			    return Object.keys(CONFIG.Actor.sheetClasses[str]).map(sheet => {
+			        return `CONFIG.Actor.sheetClasses.${str}["${sheet}"].cls.prototype.render`;
+			    })
+			}).flat()
+
+			const method = function (wrapped, forced, options, ...args) {
+				const renderItemPileInterface = Hooks.call(game.itempiles.CONSTANTS.HOOKS.PRE_RENDER_SHEET, this.document, forced, options) === false;
+				if (this._state > Application.RENDER_STATES.NONE) {
+					if (renderItemPileInterface) {
+						wrapped(forced, options, ...args)
+					} else {
+						return wrapped(forced, options, ...args)
+					}
+				}
+				if (renderItemPileInterface) return;
+				return wrapped(forced, options, ...args);
+			};
+
+			for(const override of sheetOverrides){
+				libWrapper.register("item-piles-cosmere-rpg", override, method, libWrapper.MIXED);
+			}
+		}
     };
     game.itempiles.API.addSystemIntegration(config, 'latest')
 });
